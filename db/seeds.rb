0cf8662b72e955a77299be5efc60e7d33aa341b8
Jennifer Tchai <jentchai@gmail.com>
2022-03-18 12:31:07 -0400

    added user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,19 @@ Meetup.destroy_all
 UserMeetup.destroy_all
 Comment.destroy_all
 UserTopic.destroy_all
+
+puts "Seeding users..."
+
+joe = User.create(first_name: "Joe", email: "Joe@joe.com", password: "j")
+amy = User.create(first_name: "Amy", email: "Amy@amy.com", password: "a")
+bob = User.create(first_name: "Bob", email: "Bob@bob.com", password: "b")
+joy = User.create(first_name: "Joy", email: "Joy@joy.com", password: "j")
+jim = User.create(first_name: "Jim", email: "Jim@jim.com", password: "j")
+liz = User.create(first_name: "Liz", email: "Liz@liz.com", password: "l")
+
+ana = User.create(first_name: "Ana", email: "Ana@ana.com", password: "a")
+dan = User.create(first_name: "Dan", email: "Dan@dan.com", password: "d")
+cat = User.create(first_name: "Cat", email: "Cat@cat.com", password: "c")
+sam = User.create(first_name: "Sam", email: "Sam@sam.com", password: "s")
+ron = User.create(first_name: "Ron", email: "Ron@ron.com", password: "r")
+vee = User.create(first_name: "Vee", email: "Vee@vee.com", password: "v")
