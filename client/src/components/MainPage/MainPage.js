980c784e6e472e2435eb716be734d236c9fce3e1
Jennifer Tchai <jentchai@gmail.com>
2022-03-19 11:36:37 -0400

    fixing main page and added top navbar and three card container

--- client/src/components/MainPage/MainPage.js
+++ client/src/components/MainPage/MainPage.js
@@ -1,7 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import NavBar from "../NavBar/NavBar";
+import Footer from "../Footer/Footer";
+import SearchContainer from "../SearchContainer/SearchContainer";
+import AllThree from "../AllThree/AllThree";
+
+const userId = 0;
 
 export default function MainPage() {
-  return (
-    <div>MainPage</div>
-  )
-}
+  const [meetups, setMeetups] = useState([]);
+  const [searchWord, setSearchWord] = useState("");
+  const [user, setUser] = useState({});
+  const [comments, setComments] = useState([]);
+  const [clickedMeetup, setClickedMeetup] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/meetups")
+      .then((response) => response.json())
+      .then((meetups) => setMeetups(meetups));
+  }, []);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/users/${window.userId}`)
+      .then((response) => response.json())
+      .then((user) => setUser(user));
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/comments")
+      .then((response) => response.json())
+      .then((comments) => setComments(comments));
+  }, []);
+
+  function rerender(clickedMeetup) {
+    fetch(`http://localhost:3000/meetups/${clickedMeetup.id}`)
+      .then((response) => response.json())
+      .then((meetup) => setClickedMeetup(meetup));
+  };
+
+  function handleChange(event) {
+      setSearchWord(event)
+  };
+
+  function joinMeetup(meetup){
+      setClickedMeetup(meetup)
+  };
+
+  const filteredMeetups = meetups.filter((meeting) =>
+      meeting.topic.topic_name.includes(
+        searchWord ||
+          searchWord.charAt(0).toUpperCase() +
+            searchWord.slice(1)
+      )
+    );
+
+    return (
+      <div>
+        THIS THE MAIN PAGE
+        <NavBar
+          searchWord={searchWord}
+          handleChange={handleChange}
+        />
+        <SearchContainer />
+        <AllThree
+          meetups={meetups}
+          filteredMeetups={filteredMeetups}
+          user={user}
+          comments={comments}
+          joinMeetup={joinMeetup}
+          clickedMeetup={clickedMeetup}
+          rerender={rerender}
+        />
+        <Footer />
+      </div>
+    );
+  }
