e44c7f4bcc0680931b324bf4d7ca853ba6a47c55
Jennifer Tchai <jentchai@gmail.com>
2022-03-19 11:54:15 -0400

    added page section components

--- client/src/components/AllThree/AllThree.js
+++ client/src/components/AllThree/AllThree.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
+import LeftComponent from "../LeftComponent/LeftComponent";
+import RightComponent from "../RightComponent/RightComponent";
+import CenterComponent from "../CenterComponent/CenterComponent";
+import MeetupDetails from "../MeetupDetails/MeetupDetails";
 
-export default function AllThree() {
-  return (
-    <div>AllThree</div>
-  )
-}
+export default function AllThree({ meetups, filteredMeetups, user, comments, joinMeetup, clickedMeetup, rerender }) {
+
+    return (
+      <div className="AllThree">
+        <LeftComponent
+          meetups={meetups}
+          comments={comments}
+        />
+        <CenterComponent
+          meetups={meetups}
+          filteredMeetups={filteredMeetups}
+          joinMeetup={joinMeetup}
+        />
+        <MeetupDetails
+          clickedMeetup={clickedMeetup}
+          rerender={rerender}
+        />
+      </div>
+    );
+  }
---
+++ client/src/components/RightComponent/RightComponent.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RightComponent() {
+  return (
+    <div>RightComponent</div>
+  )
+}
