93681f9f7603adb81e8b230f7fcb43adf399d169
Jennifer Tchai <jentchai@gmail.com>
2022-03-19 11:49:13 -0400

    added search bar and added navbar code

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+// import "../src/App.css";
+import SearchBar from "../SearchBar/SearchBar";
 
-export default function NavBar() {
-  return (
-    <div>NavBar</div>
-  )
-}
+export default function NavBar({ searchWord, handleChange }) {
+  
+    return (
+    <>
+      <div className="NavBar">
+        <div className="Logo">
+        {/* <img src="/logo6.png" alt="logo" width="300" /> */}
+        </div>
+        <div className="SearchBar">
+          <SearchBar
+            searchWord={searchWord}
+            handleChange={handleChange}
+          />
+        </div>
+        <div className="NavLinks">
+          <Link to="/main">Home</Link>
+          <br></br>
+          <Link to="/profile">Profile</Link>
+          <br></br>
+          <Link to="/login">Log Out</Link>
+        </div>
+      </div>
+    </>
+    );
+  }
