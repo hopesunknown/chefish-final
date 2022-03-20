cab3f6769fea2a5b92f6a60c0f0abe8951b3bdd7
Jennifer Tchai <jentchai@gmail.com>
2022-03-20 11:47:55 -0400

    need to check rightcomponent code

--- client/src/components/RightComponent/RightComponent.js
+++ client/src/components/RightComponent/RightComponent.js
@@ -1,7 +1,52 @@
-import React from 'react'
+// double check this code
 
-export default function RightComponent() {
-  return (
-    <div>RightComponent</div>
-  )
-}
+import React, { useState } from "react";
+
+export default function RightComponent({ userTopics }) {
+
+  const [topic, setTopic] = useState("");
+
+  function deleteTopic(t) {
+    alert("Topic Deleted!");
+
+    const body = { topic_id: t.id, user_id: window.userId };
+
+    const configObj = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+
+    fetch("http://localhost:3000/usertopic/delete", configObj);
+    this.setState({
+        state: this.state,
+      });
+      setTimeout(() => {
+        this.props.rerender();
+      }, 1000);
+  };
+
+    return (
+      <div className="RightComponent">
+        <h1>Your Interests</h1>
+        {userTopics &&
+          userTopics.map((t) => {
+            return (
+               
+              <p>
+                {t.topic_name}{" "}
+                <button
+                  onClick={() => deleteTopic(t)}
+                  className="DeleteBtn"
+                >
+                  x
+                </button>
+              </p>
+            );
+          })}
+      </div>
+    );
+  }
