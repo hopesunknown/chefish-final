ccae01d46e50fa0d472decaef79767d861b43b11
Jennifer Tchai <jentchai@gmail.com>
2022-03-20 11:39:08 -0400

    added meetupcard code

--- client/src/components/MeetupCard/MeetupCard.js
+++ client/src/components/MeetupCard/MeetupCard.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
-export default function MeetupCard() {
-  return (
-    <div>MeetupCard</div>
-  )
-}
+export default function MeetupCard({ leaveMeetup, meetup, handleClick }) {
+
+    return (
+      <div>
+        <div className="MeetupCard" onClick={handleClick}>
+          <h3>{meetup.title}</h3>
+          <button className="Submit"
+          onClick={() => leaveMeetup(meetup)}
+          >Leave Meetup</button>
+        </div>
+      </div>
+    );
+  }
