c9d1779fef3daade4f4a78f5180fc7fc20e12733
Jennifer Tchai <jentchai@gmail.com>
2022-03-20 10:46:08 -0400

    added search bar code

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function SearchBar({ searchWord, handleChange }) {
-  return (
-    <div>SearchBar</div>
-  )
-}
+
+    return (
+      <div>
+        <form>
+          <label>
+          <span className="iconify" data-icon="et-magnifying-glass" data-inline="false"></span>
+          Search cooking classes:
+            <input  className="Search"
+              type="text"
+              name="name"
+              onChange={(event) => handleChange(event)}
+            />
+          </label>
+          <input type="submit" value="Submit" className="Submit" />
+        </form>
+      </div>
+    );
+  }
