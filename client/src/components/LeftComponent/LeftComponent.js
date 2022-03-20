b5c905127297c359b0043a8d612a65165b155a48
Jennifer Tchai <jentchai@gmail.com>
2022-03-20 11:36:46 -0400

    added leftcomponent marquee

--- client/src/components/LeftComponent/LeftComponent.js
+++ client/src/components/LeftComponent/LeftComponent.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import CommentCard from '../CommentCard/CommentCard'
 
-export default function LeftComponent() {
-  return (
-    <div>LeftComponent</div>
-  )
-}
+export default function LeftComponent({ comments }) {
+
+        return (
+            <div className="LeftComponent">
+                
+                <div className="ScrollingList">
+                <marquee direction="up" behavior="scroll">
+                {comments.map(comment => {
+                    return <CommentCard key={comment.id} comment={comment} />
+                })}
+                </marquee>
+                </div>
+            </div>
+        );
+    }
