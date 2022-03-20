1018b3effa3efe6d1568080886d9c34188a0c4cb
Jennifer Tchai <jentchai@gmail.com>
2022-03-20 11:41:15 -0400

    added profilenavbar code

--- client/src/components/ProfileNavBar/ProfileNavBar.js
+++ client/src/components/ProfileNavBar/ProfileNavBar.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+// import "../App.css";
 
 export default function ProfileNavBar() {
-  return (
-    <div>ProfileNavBar</div>
-  )
-}
+
+    return (
+      <div className="NavBar">
+        <div className="Logo">
+          {/* <img src="/logo6.png" alt="logo" width="300" /> */}
+        </div>
+        <div className="SearchBar"></div>
+        <div className="NavLinks">
+          <Link to="/signup">
+            Sign Up
+          </Link>
+          <br></br>
+          <Link to="/login">
+            Login
+          </Link>
+        </div>
+      </div>
+    );
+  }
