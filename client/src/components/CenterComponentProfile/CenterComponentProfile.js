0179db2f5ebdc190b199773b5f397eba8ebd2773
Jennifer Tchai <jentchai@gmail.com>
2022-03-20 11:33:37 -0400

    added centercomponentprofile code

--- client/src/components/CenterComponentProfile/CenterComponentProfile.js
+++ client/src/components/CenterComponentProfile/CenterComponentProfile.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
 
-export default function CenterComponentProfile() {
-  return (
-    <div>CenterComponentProfile</div>
-  )
-}
+export default function CenterComponentProfile({ userDetails, handleClick }) {
+
+    return (
+      <div className="CenterComponentProfile">
+        <div className="SpacingDiv">
+          <div className="UserProfile">
+            <h1>Welcome back {userDetails.first_name}!</h1>
+            <h2>Email: {userDetails.email}</h2>
+
+            <p>Have a cooking class in mind? Create one:</p>
+            <button className="SignInBtn" onClick={handleClick}>
+              Create a New Cooking Class
+            </button>
+            <br/>  <br/>
+            <p>Thanks for using Chefish!</p>
+            <p>
+              Here are the classes you have joined and the cuisines you've chosen.
+            </p>
+            <br />
+            <p style={{fontStyle:"italic"}}>Stay tuned for more app features in the near future</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
