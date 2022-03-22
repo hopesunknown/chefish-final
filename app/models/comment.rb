aaab18fa571ef86563a53beef46b22e2172236cf
Jennifer Tchai <jentchai@gmail.com>
2022-03-22 00:01:34 -0400

    commented out validations for now

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
     belongs_to :meetup, dependent: :destroy
     belongs_to :user, dependent: :destroy
 
-    # validates :content, presence: true
+    # validates :content, presence: true, length: {minimum: 2}
     # validates :rating, numericality: true
 end
