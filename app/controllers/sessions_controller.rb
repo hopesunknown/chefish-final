736e65cfd2849c558b166fd5b114eb60068dac4f
Jennifer Tchai <jentchai@gmail.com>
2022-03-24 22:50:11 -0400

    brought back sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,44 @@
 class SessionsController < ApplicationController
-    # skip_before_action :authorize, only: :login
+    skip_before_action :authorize, only: :create
 
-    def login
-        user = User.find_by(first_name: params[:first_name])
-        # if user&.authenticate(params[:password])
-        #     session[:user_id] = user.id
-        #     render json: user, status: :created
-        # else 
-        #     render json: {message: "Invalid username or password"}, status: :unauthorized
-        # end 
-    # end
-    if user && user.password === params[:password]
-        session[:id] = user.id
-
-        render json: user
-    else 
-        render json: {message: "error"}
+    def create
+        user = User.find_by(username: params[:username])
+       
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else 
+            render json: {errors: ["Invalid username or password"] }, status: :unauthorized
+        end 
     end 
-end
 
-    def logout
+    def destroy
         session.delete :user_id
         head :no_content
     end
+    
+    # skip_before_action :authorize, only: :login
+
+#     def login
+#         user = User.find_by(first_name: params[:first_name])
+#         # if user&.authenticate(params[:password])
+#         #     session[:user_id] = user.id
+#         #     render json: user, status: :created
+#         # else 
+#         #     render json: {message: "Invalid username or password"}, status: :unauthorized
+#         # end 
+#     # end
+#     if user && user.password === params[:password]
+#         session[:id] = user.id
+
+#         render json: user
+#     else 
+#         render json: {message: "error"}
+#     end 
+# end
+
+#     def logout
+#         session.delete :user_id
+#         head :no_content
+#     end
 end
