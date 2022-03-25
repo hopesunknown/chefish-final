c74ce3997dfb0f2172fd3269598674f810cd5c52
Jennifer Tchai <jentchai@gmail.com>
2022-03-25 11:38:15 -0400

    changed user migration table to password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_18_162754) do
+ActiveRecord::Schema.define(version: 2022_03_25_153421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2022_03_18_162754) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
