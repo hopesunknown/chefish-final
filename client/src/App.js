9b63f8137b5abf9add5f700b42e439444ca3a7ce
Jennifer Tchai <jentchai@gmail.com>
2022-03-24 23:06:28 -0400

    adding auth to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Signup from "../src/components/Signup/Signup";
 import Signin from "../src/components/Signin/Signin";
 import Profile from "../src/components/Profile/Profile";
@@ -9,23 +9,14 @@ import MainPage from "../src/components/MainPage/MainPage";
 function App() {
   const [user, setUser] = useState(null);
   const [meetups, setMeetups] = useState([]);
-  // const [currentUser, setCurrentUser] = useState({})
+
+  const navigate = useNavigate()
 
   // useEffect(() => {
-  //   fetch("/users")
-  //   .then((response) => {
-  //     if (response.ok) {
-  //       response.json()
-  //   .then((user) => setUser(user));
-  //     }
-  //   });
+  //   fetch("/me")
+  //     .then((response) => response.json())
+  //     .then((user) => setUser(user));
   // }, []);
-
-  useEffect(() => {
-    fetch("/me")
-      .then((response) => response.json())
-      .then((user) => setUser(user));
-  }, []);
 
   useEffect(() => {
     fetch("/meetups")
@@ -33,36 +24,66 @@ function App() {
       .then((meetups) => setMeetups(meetups));
   }, []);
 
-  // function handleLogin(value){
-  //   setCurrentUser(value)
-  //   localStorage.setItem('id', value.id)
-  // }
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  function handleLogOutClick(){
+    fetch("/logout",{
+        method: "DELETE"
+    }).then((r) => {
+        if(r.ok){
+            setUser(null);
+        }
+    });
+    // Navigate to home page after logout and clear history
+    navigate("/");
+}
+
+  if (!user) return (
+    <>
+    <Container>
+      <Alert className="mt-3" variant="primary" >Please Login OR Signup To Create A New Account</Alert>
+    </Container>
+    <Signin setUser={setUser}/>
+    </>
+  )
+
   return (
     <>
       <div className="App">
-        {/* <MainPage /> */}
         
         <Routes>
           <Route exact path="/signin" element={<Signin 
           user={user} 
           setUser={setUser} />} />
+
           <Route exact path="/profile" element={<Profile 
           user={user} 
           setUser={setUser} 
           meetups={meetups} 
           setMeetups={setMeetups} />} />
+
           <Route exact path="/main" element={<MainPage 
           user={user} 
           setUser={setUser} 
           meetups={meetups} 
           setMeetups={setMeetups} />} />
+
           <Route exact path="/signup" element={<Signup 
           user={user} 
           setUser={setUser} />} />
+
           <Route exact path="/" element={<Signin 
           user={user} 
           setUser={setUser} />} />    
         </Routes>
+        
       </div>
     </>
   );
