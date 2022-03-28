89391f8bf9d5db3a80b1d05be0f6989794ce7999
Jennifer Tchai <jentchai@gmail.com>
2022-03-28 12:25:59 -0400

    searchbar code

--- client/src/components/Inspo/SearchBar.js
+++ client/src/components/Inspo/SearchBar.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
-export default function SearchBar() {
-  return (
-    <div>SearchBar</div>
-  )
-}
+export default function SearchBar({ handleSubmit, query, isLoading, setQuery }) {
+        return (
+            <form onSubmit={handleSubmit}>
+                <input 
+                    value={query}
+                    className="form-control"
+                    placeholder="Search Recipe"
+                    name="query"
+                    disabled={isLoading}
+                    onChange={(event) => setQuery(event.target.value)}
+                />   
+                <input
+                    disabled={isLoading || !query}
+                    type="submit"
+                    className="btn"
+                    value="Search"
+                />
+            </form>
+        )
+    };
