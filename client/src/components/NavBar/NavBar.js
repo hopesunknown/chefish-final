439b8118cde338768706a0391345fd2c9d44c4fc
Jennifer Tchai <jentchai@gmail.com>
2022-03-27 20:53:18 -0400

    added recipes link to navbar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -24,9 +24,11 @@ export default function NavBar({ searchWord, handleChange, userInfo, user, setUs
           className="navlinks"
           style={{ paddingRight: 10 }}
           >
-              <Nav.Link href="/main">Home</Nav.Link>&nbsp;&nbsp;&nbsp;&nbsp;
+              <Nav.Link href="/">Home</Nav.Link>&nbsp;&nbsp;&nbsp;&nbsp;
 
               <Nav.Link href="/profile">Account</Nav.Link>&nbsp;&nbsp;&nbsp;&nbsp;
+
+              <Nav.Link href="/recipes">Recipes</Nav.Link>&nbsp;&nbsp;&nbsp;&nbsp;
 
               <Nav.Link onClick={handleLogOutClick}>Logout</Nav.Link>&nbsp;&nbsp;&nbsp;&nbsp;
           </Nav>
