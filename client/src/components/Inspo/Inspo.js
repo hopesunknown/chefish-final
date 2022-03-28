da31b8c9f5bd31185a6063edbec7b2abc886e128
Jennifer Tchai <jentchai@gmail.com>
2022-03-28 11:48:21 -0400

    fixing meal fetch

--- client/src/components/Inspo/Inspo.js
+++ client/src/components/Inspo/Inspo.js
@@ -1,41 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MealItem from "./MealItem";
 // import './style.css';
 
 export default function Inspo() {
     const [search, setSearch] = useState("");
-    const [Mymeal,setMeal] = useState();
-    const searchMeal=(evt)=>{
-        if(evt.key=="Enter")
-        {
-            fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
-            .then(res=>res.json())
-            .then(data=> {setMeal(data.meals);setSearch("")})
+    const [meal, setMeal] = useState();
+
+    // function searchMeal(evt) {
+    //     if(evt.key === "Enter")
+    //     {
+
+    useEffect(() => {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+        .then(res => res.json())
+        .then(data => {setMeal(data.meals);setSearch("")})
+        }, []);
+
+        console.log(meal);
+
+        function searchMeal(event) {
+            if (event.key === "Enter") {
+
+            }
         }
-    }
+
     return (
         <>
-            <div className="main">
+            <div className="main-inspo">
                 <div className="heading">
-                    <h1>Search Your Food Recipe</h1>
+                    <h1>Search Recipes</h1>
                     <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque tempore unde sed ducimus voluptates illum!</h4>
                 </div>
                 <div className="searchBox">
                     <input 
-                    type="search" className="search-bar" onChange={(e)=>setSearch(e.target.value)} 
+                    type="search" className="search-bar" onChange={(e) => setSearch(e.target.value)} 
                     value={search} 
                     onKeyPress={searchMeal}/>
                 </div>
                 <div className="container">
                    {   
-                  
-                    (Mymeal==null)? <p className="notSearch">Not found</p> : 
-                         Mymeal.map((res)=>{
+                    (meal === null) ? (<p className="notSearch">Not found</p>) : 
+                         (meal.map((res) => {
                              return(
-                            <MealItem data={res}/>)} 
-                     
-                    ) 
-                   
+                            <MealItem data={res}/>)}))
                    }
                 </div>
             </div>
