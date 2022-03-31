76a6a599e14f562d5c8e9b23f35204a5c24dd2ee
Jennifer Tchai <jentchai@gmail.com>
2022-03-30 21:26:19 -0400

    adding banners

--- client/src/components/MainPage/MainPageBanner.js
+++ client/src/components/MainPage/MainPageBanner.js
@@ -3,6 +3,10 @@ import React from 'react'
 export default function () {
   return (
     <div className="main-page-banner">
+        <div className="main-banner-info">
+            meet new people<br/>
+            learn new things<br/>
+        </div>
     </div>
   )
 }
