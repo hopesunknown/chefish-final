43f9b1eab3f8d53d88777223d2e5231475abaebf
Jennifer Tchai <jentchai@gmail.com>
2022-03-31 00:00:52 -0400

    updated mainpagebanner

--- client/src/components/MainPage/MainPageBanner.js
+++ client/src/components/MainPage/MainPageBanner.js
@@ -4,8 +4,12 @@ export default function MainPageBanner () {
   return (
     <div className="main-page-banner">
         <div className="main-banner-info">
-            meet new people<br/>
-            learn new things<br/>
+            meet. learn. eat.<br/>
+            <div className="main-banner-info-2">
+                At Chefish, we want to help foodies and food enthusiasts meet others,<br/>
+                 make friends, learn from each other, try new dishes, and explore their culinary interests.<br/>
+                Find a meetup near you and join the fun.
+            </div>
         </div>
     </div>
   )
